b349b8441aac0a70323d2593deffd9164d78341d
Gabriel Furquim <gabriel.furquim@outlook.com>
2024-01-25 15:50:43 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatButtonModule],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+}
